f6fe2c01c0367ad3a61151980444d41936dff7b7
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-11-07 19:28:57 -0600

    test to check of Engineer github is working works

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -6,3 +6,8 @@ expect(engineer.name).toBe('Dan')
 }
 )
 
+test('github is entered to engineer', () => {
+let engineer = new Engineer('Dan','2345','d@a', 'gitdreams')
+expect(engineer.github).toBeDefined()
+}
+)
